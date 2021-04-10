ffb6bf6ff5fa043b6564ff493158c5598b15c65a
Wonderchn <46928995+Wonderchn@users.noreply.github.com>
2021-04-10 19:26:34 +0800

    二分查找

--- src/main/java/com/hongna/leetcode/Search/binarySearch.java
+++ src/main/java/com/hongna/leetcode/Search/binarySearch.java
@@ -2,7 +2,21 @@ package com.hongna.leetcode.Search;
 
 public class binarySearch {
 
-    public int binarySearch(int []datasource){
+    public int binarySearch(int []datasource,int searchNumber){
+        int left = 0 ;
+        int right = datasource.length -1;
+        while (left <= right){
+            int mid = (right + left)/2;
+            if (datasource[mid] == searchNumber){
+                return mid;
+            }
+            else if (searchNumber < datasource[mid] ){
+                right = mid - 1;
+            }
+            else if (searchNumber > datasource[mid]){
+                left = mid + 1;
+            }
+        }
         return -1;
     }
 }
