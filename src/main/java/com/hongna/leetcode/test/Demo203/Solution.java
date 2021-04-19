084961bdfc40f758b35f2a25c9593e2088c8b219
Wonderchn <46928995+Wonderchn@users.noreply.github.com>
2021-04-20 00:03:10 +0800

    test

---
+++ src/main/java/com/hongna/leetcode/test/Demo203/Solution.java
@@ -0,0 +1,18 @@
+package com.hongna.leetcode.test.Demo203;
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else prev = prev.next;
+        }
+        return head;
+    }
+}
