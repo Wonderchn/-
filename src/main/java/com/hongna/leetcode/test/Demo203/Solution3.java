a8a75dafbc8d4bcd20583bdd6d7996ed60a9af46
Wonderchn <46928995+Wonderchn@users.noreply.github.com>
2021-04-20 20:32:31 +0800

    test

---
+++ src/main/java/com/hongna/leetcode/test/Demo203/Solution3.java
@@ -0,0 +1,25 @@
+package com.hongna.leetcode.test.Demo203;
+
+public class Solution3 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null){
+            return null;
+        }
+        ListNode nextNode = removeElements(head.next, val);
+        if (head.val == val){
+            return nextNode;
+        }
+        else {
+            head.next = nextNode;
+            return head;
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+        ListNode res = new Solution3().removeElements(head, 6);
+        System.out.println(res);
+    }
+}
