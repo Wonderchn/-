a9a419ff35c9e0f14a32b02a7a2aaf3e1a726ecf
Wonderchn <46928995+Wonderchn@users.noreply.github.com>
2021-05-04 01:10:47 +0800

    test

---
+++ src/main/java/com/hongna/leetcode/Tree/BinarySearchTree/BST.java
@@ -0,0 +1,82 @@
+package com.hongna.leetcode.Tree.BinarySearchTree;
+
+public class BST <E extends Comparable<E>>{
+    private class Node{
+        private E e;
+        private Node left, right;
+        public Node(E e){
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+    private Node root;
+    private int size;
+
+    public BST(){
+        root = null;
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+
+    // 向二分搜索树中添加新的元素e
+    public void add(E e){
+        root = add(root, e);
+
+    }
+
+    //向以node为根的二分搜索树中插入元素E,递归算法
+    //  返回插入新节点后  当前二分搜索树的根
+    private Node add(Node node, E e){
+        if (node == null){
+            size++;
+            return new Node(e);
+        }
+        if (e.compareTo(node.e)<0){
+           node.left =  add(node.left, e);
+        }
+        // e. compartTo(node.e) >0
+        else if(e. compareTo(node.e) >0)
+            {
+            node.right = add(node.right,e);
+
+        }
+        return node;
+    }
+
+    public boolean contains(E e){
+        return contains(root,e);
+    }
+
+    /**
+     * 此方法表明的是在以当前的node节点下查询 e的值
+     * 如果查到 返回true
+     * 否则 false
+     * @param node
+     * @param e
+     * @return
+     */
+    public boolean contains(Node node , E e){
+        if (node == null){
+            return false;
+        }
+        if (e.compareTo(node.e) < 0){
+            return contains(node.left, e);
+
+        }
+        else if(e.compareTo(node.e) < 0){
+            return contains(node.left,e);
+        }
+        else {
+            return contains(node.right,e);
+        }
+    }
+}
