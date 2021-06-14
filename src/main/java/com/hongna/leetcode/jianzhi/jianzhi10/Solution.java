4f4d159b6517acf42b05e842561f5ca2b3cada17
Wonderchn <46928995+Wonderchn@users.noreply.github.com>
2021-06-15 00:58:05 +0800

    test

--- src/main/java/com/hongna/leetcode/jianzhi/jianzhi10/Solution.java
+++ src/main/java/com/hongna/leetcode/jianzhi/jianzhi10/Solution.java
@@ -2,6 +2,16 @@ package com.hongna.leetcode.jianzhi.jianzhi10;
 
 class Solution {
     public int fib(int n) {
-        return -1;
+       if (n<2){
+           return n;
+       }
+       long[] result = new long[n+1];
+       result[0] = 0;
+       result[1] = 1;
+       for (int i =2 ; i<=n ; i++){
+           result[i] = result[i - 1]+result[i-2];
+           result[i] %= (Math.pow(10,9))+7;
+       }
+       return (int)result[n];
     }
 }
