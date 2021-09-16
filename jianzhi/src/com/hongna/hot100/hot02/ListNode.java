2723ff3a15b00a7800e25e60a4ac8bcf610a6820
小明 <a1073506500@qq.com>
2021-09-16 22:58:24 +0800

    TOP100-02 两数相加

---
+++ jianzhi/src/com/hongna/hot100/hot02/ListNode.java
@@ -0,0 +1,12 @@
+package com.hongna.hot100.hot02;
+
+public class ListNode {
+
+
+   int val;
+     ListNode next;
+     ListNode() {}
+    ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+}
