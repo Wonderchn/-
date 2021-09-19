98529184746b6d18242a432a435663d5b9ef5f55
小明 <a1073506500@qq.com>
2021-09-19 23:46:31 +0800

    剑指offer 09 - 用两个栈实现队列

--- jianzhi/src/com/hongna/jianzhi/jianzhi09/CQueue.java
+++ jianzhi/src/com/hongna/jianzhi/jianzhi09/CQueue.java
@@ -1,14 +1,16 @@
 package com.hongna.jianzhi.jianzhi09;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Stack;
 
 class CQueue {
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
 
     public CQueue() {
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
+        stack1 = new LinkedList<Integer>();
+        stack2 = new LinkedList<Integer>();
     }
 
     public void appendTail(int value) {
@@ -16,17 +18,21 @@ class CQueue {
     }
 
     public int deleteHead() {
-        if (!stack2.isEmpty()) {
-            return stack2.pop();
-        } else {
+        // 如果第二个栈为空
+        if (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
-
         }
-        return stack2.isEmpty() ? -1 : stack2.pop();
+        if (stack2.isEmpty()) {
+            return -1;
+        } else {
+            int deleteItem = stack2.pop();
+            return deleteItem;
+        }
     }
 }
+
 
 
 /**
