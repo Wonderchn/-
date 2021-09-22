fa32691beb9468ed70feb0d5c32664e42ca190d5
小明 <a1073506500@qq.com>
2021-09-22 14:10:41 +0800

    剑指offer 30 - 包含Min函数的栈

--- jianzhi/src/com/hongna/jianzhi/jianzhi30/Solution.java
+++ jianzhi/src/com/hongna/jianzhi/jianzhi30/Solution.java
@@ -2,6 +2,10 @@ package com.hongna.jianzhi.jianzhi30;
 
 import java.util.Stack;
 
+/**
+ * 这题的思路是维护一个辅助栈
+ * 当我们每次新加一个元素的时候
+ */
 public class Solution {
     class MinStack {
         Stack<Integer> A, B;
