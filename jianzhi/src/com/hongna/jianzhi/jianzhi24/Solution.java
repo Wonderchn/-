305fe16cd66b05ae7cc783128f730a52c38476bf
小明 <a1073506500@qq.com>
2021-09-24 12:57:54 +0800

    剑指offer 24 - 反转链表

--- jianzhi/src/com/hongna/jianzhi/jianzhi24/Solution.java
+++ jianzhi/src/com/hongna/jianzhi/jianzhi24/Solution.java
@@ -2,6 +2,7 @@ package com.hongna.jianzhi.jianzhi24;
 
 class Solution {
     //反转链表
+    //假设我们处理的节点后面的链表是已经被反转过得
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode cur = head;
